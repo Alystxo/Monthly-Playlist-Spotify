7997b670fca9fec70eea3baa9d2535191219c6a1
Alystxo <81914479+Alystxo@users.noreply.github.com>
2021-04-05 17:32:36 +0200

    Finishing the function!

--- src/js/sort.js
+++ src/js/sort.js
@@ -1,37 +1,25 @@
-console.log("test");
-var abctest=69420;
-var deftest=911;
-console.log(abctest-deftest);
-function ghitest (input){
-    input=input+"-test"
-console.log(input)
+function sortSongs (input){
+
+    //Array for results. 
+    var tracks = [];
+
+    input.items.forEach(element => {
+        //Defining dates. 
+        var added_at = new Date(element.added_at)
+        var date = (added_at.toLocaleString('default', { month: 'long', year: '2-digit'}))
+
+        //Filling the array with tracks and dates. 
+        if (tracks[date]) {
+            tracks[date].push(element.track.uri);
+        } else {
+            tracks[date] = [element.track.uri];
+        }
+        
+    });
+
+    console.log(tracks)
 }
-ghitest("jkl")
-ghitest("mno")
 
-
-console.log(data.items[0].track.album.name)
-
-// Ich bin nur ein Kommentar. Keine Sorge. Was hier steht wird nicht ausgeführt.
-
-/**
- *  Geht auch so!
- */
-
-/**
- * Ziel für heute: Umgang mit tieferen Arrays, Callbacks (again), wie man alle Items eines Arrays einzelnd verarbeitet und Dates
- */
-
-//Schritt 1: Durch die Song Items loopen
-
-//Schritt 2: Lass dir das Datum und die Song URI von jeden Song ausgeben
-
-//Schritt 3: Speicher alle Song URIs in einem Array;
-
-//Schritt 4: Formatiere das Datum in eine bessere lesbare Form um.
-
-//Schritt 5: Sortiere die Song URIs nach den jeweiligen Datum in eine Array.
-
-//Schritt 6: Fass alles was du gemacht hast in eine sortSongs(data) Funktion zusammen.
+sortSongs(data)
 
 //Falls du noch Bock hast gibt es hier bei Exercises: https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_variables1
