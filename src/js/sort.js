dcbb304110d8ce2931fe57f7b501025e4aa9e2cb
Marco Sadowski <marco@camefrom.space>
2021-04-06 23:39:24 +0200

    sort some things out and edited login button

--- src/js/sort.js
+++ src/js/sort.js
@@ -1,25 +1,19 @@
-function sortSongs (input){
+function sortSongs(input) {
 
     //Array for results. 
     var tracks = [];
 
     input.items.forEach(element => {
         //Defining dates. 
-        var added_at = new Date(element.added_at)
-        var date = (added_at.toLocaleString('default', { month: 'long', year: '2-digit'}))
+        var added_at = new Date(element.added_at);
+        var date = (added_at.toLocaleString('default', { month: 'long', year: '2-digit'}));
 
         //Filling the array with tracks and dates. 
         if (tracks[date]) {
             tracks[date].push(element.track.uri);
         } else {
             tracks[date] = [element.track.uri];
-        }
-        
+        } 
     });
-
-    console.log(tracks)
+    console.log(tracks);
 }
-
-sortSongs(data)
-
-//Falls du noch Bock hast gibt es hier bei Exercises: https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_variables1
